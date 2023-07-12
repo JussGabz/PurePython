b3f8393a43fb34635da802ae917744329883dc87
JussGabz <gabrieli94@hotmail.co.uk>
2023-07-12 15:32:10 +0100

    new workout plan fixture

--- test_workoutplan.py
+++ test_workoutplan.py
@@ -3,14 +3,16 @@ from exercise import Exercise
 import pytest
 
 @pytest.fixture
-def new_workout_plan():
-    new_workout_plan = WorkoutPlan(name="New Workout Plan", exercises=[], difficulty="Hard", date_created="Today")
-    return new_workout_plan
-
-@pytest.fixture
 def new_exercise():
     new_exercise = Exercise(name="New Exercise", target_area="New Target Area")
     return new_exercise
+
+@pytest.fixture
+def new_workout_plan(new_exercise):
+    new_workout_plan = WorkoutPlan(name="New Workout Plan", exercises=[new_exercise], difficulty="Hard", date_created="Today")
+    return new_workout_plan
+
+
 
 def test_workoutplan_name(new_workout_plan):
     assert new_workout_plan.get_workoutplan_name() == "New Workout Plan"
@@ -18,17 +20,12 @@ def test_workoutplan_name(new_workout_plan):
 def test_workoutplan_difficulty(new_workout_plan):
     assert new_workout_plan.get_difficulty() == "Hard"
 
-def test_workoutplan_exercises(new_workout_plan):
-    assert new_workout_plan.get_exercises() == []
+def test_workoutplan_exercises(new_workout_plan, new_exercise):
+    assert new_workout_plan.get_exercises() == [new_exercise]
 
 def test_workoutplan_creation_date(new_workout_plan):
     assert new_workout_plan.get_date_created() == "Today"
 
-def test_add_exercise(new_workout_plan):
+def test_add_exercise(new_workout_plan, new_exercise):
     new_workout_plan.add_exercise(new_exercise)
-    assert new_workout_plan.exercises == [new_exercise]
-
-def test_add_exercise_existing(new_workout_plan):
-    new_workout_plan.add_exercise(new_exercise)
-    new_workout_plan.add_exercise(new_exercise)
-    assert new_workout_plan.get_exercises() == [new_exercise, new_exercise]
+    assert new_workout_plan.exercises == [new_exercise, new_exercise]
