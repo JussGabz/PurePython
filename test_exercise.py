94a5aad22f0e6d2923a0aea2fdac746ab65d6543
JussGabz <gabrieli94@hotmail.co.uk>
2023-07-12 12:22:01 +0100

    add test for excerise + workoutplan

--- test_exercise.py
+++ test_exercise.py
@@ -1,3 +1,10 @@
 from exercise import Exercise
 
 new_exercise = Exercise(name="New Exercise", target_area="Upper Body")
+
+def test_get_exercise_name():
+    assert new_exercise.name
+
+def test_get_target_area():
+    assert new_exercise.target_area
+
--- test_workoutplan.py
+++ test_workoutplan.py
@@ -1,21 +1,34 @@
 from workoutplan import WorkoutPlan
+from exercise import Exercise
+import pytest
 
+@pytest.fixture
+def new_workout_plan():
+    new_workout_plan = WorkoutPlan(name="New Workout Plan", exercises=[], difficulty="Hard", date_created="Today")
+    return new_workout_plan
 
-new_wo_plan = WorkoutPlan(name="New Workout Plan", exercises=[], difficulty="Hard", date_created="Today")
+@pytest.fixture
+def new_exercise():
+    new_exercise = Exercise(name="New Exercise", target_area="New Target Area")
+    return new_exercise
 
-def test_workoutplan_name():
-    assert new_wo_plan.get_workoutplan_name() == "New Workout Plan"
+def test_workoutplan_name(new_workout_plan):
+    assert new_workout_plan.get_workoutplan_name() == "New Workout Plan"
 
-def test_workoutplan_difficulty():
-    assert new_wo_plan.get_difficulty() == "Hard"
+def test_workoutplan_difficulty(new_workout_plan):
+    assert new_workout_plan.get_difficulty() == "Hard"
 
-def test_workoutplan_exercises():
-    assert new_wo_plan.get_exercises() == []
+def test_workoutplan_exercises(new_workout_plan):
+    assert new_workout_plan.get_exercises() == []
 
-def test_workoutplan_creation_date():
-    assert new_wo_plan.get_date_created() == "Today"
+def test_workoutplan_creation_date(new_workout_plan):
+    assert new_workout_plan.get_date_created() == "Today"
 
+def test_add_exercise(new_workout_plan):
+    new_workout_plan.add_exercise(new_exercise)
+    assert new_workout_plan.exercises == [new_exercise]
 
-
-
-              
+def test_add_exercise_existing(new_workout_plan):
+    new_workout_plan.add_exercise(new_exercise)
+    new_workout_plan.add_exercise(new_exercise)
+    assert new_workout_plan.get_exercises() == [new_exercise, new_exercise]
