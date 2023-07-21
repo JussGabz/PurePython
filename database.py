a5e7714f88d4af2e907d006b51fd272368ee922c
JussGabz <gabrieli94@hotmail.co.uk>
2023-07-21 14:06:13 +0100

    primary key change + insert commands

--- database.py
+++ database.py
@@ -1,61 +1,92 @@
 import psycopg2
+from psycopg2 import OperationalError
 
 def database():
-    # Connect to Postgres DB
-    conn = psycopg2.connect(
-        database="postgres", user="postgres", password="admin")
-    
-    conn.autocommit = True
-    
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
 
-    # Check If Database exists
-    cur.execute("""
-                SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'gymapp'
-                """)
+    print("Set Up GymApp Database..")
+    try:
+        # Connect to Postgres DB
+        conn = psycopg2.connect(
+            database="postgres", user="postgres", password="admin")
+        
+        conn.autocommit = True
+        
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
+
+        # Check If Database exists
+        cur.execute("""
+                    SELECT 1 FROM pg_catalog.pg_database WHERE datname = 'gymapp'
+                    """)
+        
+        exists = cur.fetchone()
+
+        if not exists:
+            # Create initial GYM APP Database
+            cur.execute("CREATE DATABASE gymapp")
+
+        # Connect to Postgres DB
+        conn = psycopg2.connect(
+            database="gymapp", user="postgres", password="admin")
+        
+        # Open a cursor to perform database operations
+        cur = conn.cursor()
+        conn.autocommit = True
+
+        # Create Exercise Table
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS exercise(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                target_area VARCHAR(50) NOT NULL,
+                difficulty VARCHAR(50) NOT NULL);""")
+
+        # Create Workout Plan
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS workoutplan(
+                id SERIAL PRIMARY KEY,
+                name VARCHAR(50) NOT NULL,
+                exercises VARCHAR(50) NOT NULL,
+                workout VARCHAR(50) NOT NULL,
+                date_created DATE)
+            """)
+        
+        cur.execute(
+            """
+            CREATE TABLE IF NOT EXISTS users(
+                id SERIAL PRIMARY KEY,
+                first_name VARCHAR(50) NOT NULL,
+                last_name VARCHAR(50) NOT NULL,
+                username VARCHAR(50) NOT NULL,
+                password VARCHAR(50) NOT NULL,
+                date_created DATE)
+            """
+            )
+        print("GymApp Database Set up Successfully..")
+    except OperationalError as e:
+        print(f"Error: {e}")
+
+
+def insert_command(name, target_area, difficulty):
+    sql_command = f"INSERT INTO exercise (name, target_area, difficulty) values ('{name}', '{target_area}', '{difficulty}')"
+    return sql_command
     
-    exists = cur.fetchone()
+def insert(name:str , target_area:str, difficulty:str):
 
-    if not exists:
-        # Create initial GYM APP Database
-        cur.execute("CREATE DATABASE gymapp")
+    sql_command = f"INSERT INTO exercise (name, target_area, difficulty) values ('{name}', '{target_area}', '{difficulty}')"
 
-    # Connect to Postgres DB
-    conn = psycopg2.connect(
+    # Connect Database
+    try:
+        conn = psycopg2.connect(
         database="gymapp", user="postgres", password="admin")
-    
-    # Open a cursor to perform database operations
-    cur = conn.cursor()
-    conn.autocommit = True
 
-    # Create Exercise Table
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS exercise(
-            id INT PRIMARY KEY, target_area VARCHAR(50) NOT NULL, difficulty VARCHAR(50) NOT NULL);""")
+        cur = conn.cursor()
 
-    # Create Workout Plan
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS workoutplan(
-            id INT PRIMARY KEY,
-            name VARCHAR(50) NOT NULL,
-            exercises VARCHAR(50) NOT NULL,
-            workout VARCHAR(50) NOT NULL,
-            date_created DATE)
-        """)
-    
-    cur.execute(
-        """
-        CREATE TABLE IF NOT EXISTS users(
-            id INT PRIMARY KEY,
-            first_name VARCHAR(50) NOT NULL,
-            last_name VARCHAR(50) NOT NULL,
-            username VARCHAR(50) NOT NULL,
-            password VARCHAR(50) NOT NULL,
-            date_created DATE)
-        """
-        )
-    
-database()
+        cur.execute(sql_command)
+        conn.commit()
+        conn.close()
+        print("Data Inserted")
+    except OperationalError as e:
+        print(f"Error: {e}")
