4b4845dc156ca0c7716437a6397436b390caaf06
JussGabz <gabrieli94@hotmail.co.uk>
2023-07-21 14:05:12 +0100

    add create exercise function

--- exercise.py
+++ exercise.py
@@ -1,15 +1,25 @@
+from database import insert
+
 class Exercise:
 
     '''
     Class for Exercise Workout 
     '''
 
-    def __init__(self, name: str, target_area: str):
+    def __init__(self, name: str, target_area: str, difficulty: str):
         self.name = name
         self.target_area = target_area
+        self.difficulty = difficulty
 
     def get_exercise_name(self):
         return self.name
 
     def get_target_area(self):
         return self.target_area
+    
+    def create_exercise(self):
+        insert(
+            name=self.name,
+            target_area=self.target_area,
+            difficulty=self.difficulty
+        )
