b836c413123e5dd27da37bfb0bcbb586558b60d0
JussGabz <gabrieli94@hotmail.co.uk>
2023-08-07 11:55:20 +0100

    add menu to app and view exercise func

--- main.py
+++ main.py
@@ -1,11 +1,40 @@
-from database import database, insert
+from database import database
+from exercise import Exercise, retrieve_exercise
 
 # Run Database Setup
 database()
 
-response = input("Do you want to create an exercise?: ")
+def mainmenu():
+    response = input("Which option do you want to go for?\n1) Exercise\n2) Workout Plan\nOption: ")
 
-def start():
+    if response == '1':
+        print("Exercise!")
+        exercise_menu()
+    elif response == '2':
+        print("Workout Plan!")
+
+        
+
+def exercise_menu():
+    print(
+        """What do you want to do?
+        1) Create
+        2) View Exercise
+        3) Remove Exercise
+        4) Update Exercise""")
+    exercise_opt = input("Option: ")
+
+    if exercise_opt == "1":
+        create_exercise()
+    elif exercise_opt == "2":
+        view_exercise()
+
+
+def workout_plan_menu():
+    pass
+
+
+def create_exercise():
     exercise_name = input("Exercise Name?: ")
     target_area = input("What area of the body is this targeting?: ")
     difficulty = input("How difficult is this exercise?: ")
@@ -15,14 +44,37 @@ def start():
     )
     if valid == "Y" or "y":
         print("Confirmed")
-        insert(exercise_name, target_area, difficulty)
+        exercise = Exercise(exercise_name, target_area, difficulty)
+        exercise.save_exercise()
     elif valid == "N" or "n":
-        start()
+        mainmenu()
+
+def view_exercise():
+    exercise_name = input("What exercise detail do you want? ")
+    exercises = retrieve_exercise(exercise_name)
+    for exercise in exercises:
+        id, name, target_area, difficulty = exercise
+        print("___________________________")
+        print("ID:", id)
+        print("Name:", name)
+        print("Target Area:", target_area)
+        print("Difficulty:", difficulty)
+        print("___________________________")
+
+
     
 
+mainmenu()
 
-if response == "Y" or "y":
-    start()
+# response = input("Do you want to create an exercise?: ")
+
+# if response == "Y" or "y":
+#     start()
+
+
+
+
+
 
 
 
